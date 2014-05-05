ecae4fd9047026de9324be8767a1d9a3079dff51
wdanxna <wdanxna@gmail.com>
2014-05-05 10:26:44 +0800

    init

---
+++ EXIFOperator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EXIFOperator
+//
+//  Created by bravo on 14-4-30.
+//  Copyright (c) 2014年 bravo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EXIFOperator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EXIFOperator
+//
+//  Created by bravo on 14-4-30.
+//  Copyright (c) 2014年 bravo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
